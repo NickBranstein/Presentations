8682258939fe57854a687951606ad8693b6a301c
NickBranstein <nick@brosteins.com>
2017-09-05 15:55:44 -0400

    more jokes

--- 2017-DogFoodCon-NativeScript/jokes-ng/src/app/services/jokeService.ts
+++ 2017-DogFoodCon-NativeScript/jokes-ng/src/app/services/jokeService.ts
@@ -13,12 +13,15 @@ export class JokeService{
             new Joke(4, 'What\'s E.T. short for?', 'Because he\'s got little legs.'),
             new Joke(5, 'Have you ever tried eating a clock?', 'It\'s really time consuming.'),
             new Joke(6, 'Why does a chicken coop have two doors?', 'If it had four it would be a chicken sedan.'),
-            new Joke(7, 'How do you fix a jack-o-lantern?', 'With a pumpkin patch.'),
+            new Joke(7, 'What\'s the best thing about UDP jokes?', 'I don\'t care if you get them'),
             new Joke(8, 'Why can\'t you trust an atom?', 'Because they make up everything.'),
             new Joke(9, 'What\'s the object-oriented way to become wealthy?', 'Inheritance.'),
-            new Joke(10, 'What is a programmer\'s favorite hangout place?', 'Foo Bar.')
+            new Joke(10, 'What is a programmer\'s favorite hangout place?', 'Foo Bar.'),
+            new Joke(11, 'What do you call an alligator wearing a vest?', 'An investigator.'),
+            new Joke(12, 'What does a horse call the person who lives next door to him?', 'His neeeeeeeeeeeeeeeighbor!'),
+            new Joke(13, 'What\'s the best thing about TCP jokes?', 'I get to keep telling them until you get them.'),
+            new Joke(14, 'How many programmers does it take to screw in a light bulb?', 'None, it\'s a hardware problem.')
         ];
-        
     }
 
     all(): Array<Joke>{
